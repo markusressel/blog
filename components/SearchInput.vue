510fa7b12b433febdf6ff509ecce65b65c9813e8
Markus Ressel <mail@markusressel.de>
2021-02-05 10:28:52 +0100

    search styling

--- components/SearchInput.vue
+++ components/SearchInput.vue
@@ -8,16 +8,24 @@
       class="rounded-full p-3 placeholder-gray-800 dark:placeholder-gray-200 text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-800 border-none shadow-md focus:outline-none"
     />
 
-    <div class="absolute rounded bg-red-100 floating z-100">
+    <div
+      v-if="searchQuery"
+      class="absolute right-0 mt-3 w-2/3 mr-1 rounded-xl bg-gray-100 dark:bg-gray-900 border-4 border-solid border-gray-400 dark:border-gray-700 z-100"
+    >
       <div v-if="articles.length">
-        <div v-for="article of articles" :key="article.slug" class="px-4 py-2">
+        <div v-for="article of articles" :key="article.slug" class="px-4 py-2 hover:bg-gray-300 dark-hover:bg-gray-700">
           <NuxtLink
+          style="text-decoration: none;"
+            v-on:click.native="searchQuery = ''"
             :to="{ name: 'blog-post-slug', params: { slug: article.slug } }"
           >
             <div class="font-bold">{{ article.title }}</div>
-            <div>{{ article.description }}</div>
+            <div class=" text-gray-800 dark:text-gray-200">{{ article.description }}</div>
           </NuxtLink>
         </div>
+      </div>
+      <div v-else class="px-4 py-2">
+        Nothing found 😢
       </div>
     </div>
   </div>
