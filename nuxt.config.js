4c998e47acd093c1eb2c5c1dd979a2182c31c234
Markus Ressel <mail@markusressel.de>
2021-03-10 13:03:10 +0100

    maybe fix component autoimport?

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,9 @@ export default {
   plugins: ['~/plugins/vuescroll'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+      { path: '~/components', pathPrefix: false }
+  ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
