8251276148f3a993c04d839f448717331be5b723
markusressel <markusressel@users.noreply.github.com>
2021-03-11 19:27:38 +0000

    Prettified Code!

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,9 @@
   <header class="flex justify-between py-2 h-12">
     <NuxtLink to="/" class="flex place-items-center">
       <Logo class="align-middle" />
-      <div class="text-gray-800 dark:text-gray-100 font-bold pl-4">{{ siteName }}</div>
+      <div class="text-gray-800 dark:text-gray-100 font-bold pl-4">
+        {{ siteName }}
+      </div>
     </NuxtLink>
     <div class="flex self-center items-center">
       <ColorModePicker />
