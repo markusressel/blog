dbc31194dc9b89372d21282ef6999f4e6ed276f9
Markus Ressel <mail@markusressel.de>
2021-03-11 19:28:50 +0100

    better responsiveness

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="inline-flex flex-wrap">
+  <div class="flex flex-wrap">
     <NuxtLink to="/projects"
-      class="flex place-items-center flex-col w-48 bg-gray-100 dark:bg-gray-800 rounded-xl p-4 mx-0 my-2 md:mx-4 md:my-4 shadow-lg hover:shadow-xl">
+      class="flex place-items-center flex-col w-full md:w-48 bg-gray-100 dark:bg-gray-800 rounded-xl p-4 mx-0 my-2 md:mx-4 md:my-4 shadow-lg hover:shadow-xl">
       <IconRocket class="fill-current h-24 w-24"/>
       <div class="text-4xl font-bold">Projects</div>
     </NuxtLink>
 
     <NuxtLink to="/blog"
-    class="flex place-items-center flex-col w-48 bg-gray-100 dark:bg-gray-800 rounded-xl p-4 mx-0 my-2 md:mx-4 md:my-4 shadow-lg hover:shadow-xl">
+    class="flex place-items-center flex-col w-full md:w-48 bg-gray-100 dark:bg-gray-800 rounded-xl p-4 mx-0 my-2 md:mx-4 md:my-4 shadow-lg hover:shadow-xl">
       <IconBlog class="fill-current h-24 w-24"/>
       <div class="text-4xl font-bold">Blog</div>
     </NuxtLink>
