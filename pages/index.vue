f9271e832866c2742eb56b2d43d5e72fbcb7bf75
markusressel <markusressel@users.noreply.github.com>
2021-03-28 17:56:47 +0000

    Prettified Code!

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,11 @@
       <div class="text-4xl font-bold">Blog</div>
     </NuxtLink>
 
-    <a href="https://github.com/markusressel" target="_blank" class="flex place-items-center flex-col w-full md:w-48 bg-gray-100 dark:bg-gray-800 rounded-xl p-4 mx-0 my-2 md:mx-4 md:my-4 shadow-lg hover:shadow-xl">
+    <a
+      href="https://github.com/markusressel"
+      target="_blank"
+      class="flex place-items-center flex-col w-full md:w-48 bg-gray-100 dark:bg-gray-800 rounded-xl p-4 mx-0 my-2 md:mx-4 md:my-4 shadow-lg hover:shadow-xl"
+    >
       <IconGithub class="fill-current h-24 w-24" />
       <div class="text-4xl font-bold">GitHub</div>
     </a>
