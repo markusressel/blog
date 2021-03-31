264555fb3d195c27ae8e21374d3f163e575ee12a
Markus Ressel <mail@markusressel.de>
2021-03-31 17:01:11 +0200

    manual fetching of prev/next to incorporate dummy filtering in production

--- pages/blog/post/_slug.vue
+++ pages/blog/post/_slug.vue
@@ -12,14 +12,39 @@ export default {
   },
   async asyncData({ $content, error, params }) {
     // fetch article data
-    const article = await getArticle($content, error, params.slug)
+    const article = await getArticle($content, error, params.slug);
 
     // fetch the previous and next article
-    const [prev, next] = await $content('articles')
+    let tmp = await $content('articles')
       .only(['title', 'slug'])
       .sortBy('createdAt', 'asc')
-      .surround(params.slug)
-      .fetch()
+      .fetch();
+    tmp = tmp.filter((keyword, index) => {
+      if (!process.env.NODE_ENV === 'production') {
+        return true;
+      } else {
+        return tmp[index].dummy === undefined ||
+          tmp[index].dummy === false;
+      }
+    });
+
+    let prev = null, next = null;
+    for (let i = 0; i <= tmp.length; i++) {
+      let item = tmp[i];
+      if (item.slug  == params.slug) {
+        if (i > 0) {
+          prev = await $content('articles', tmp[i-1].slug)
+            .only(['title', 'slug'])
+            .fetch();
+        }
+        if (i < tmp.length - 1) {
+          next = await $content('articles', tmp[i+1].slug)
+            .only(['title', 'slug'])
+            .fetch();
+        }
+        break;
+      }
+    }
 
     return {
       article,
