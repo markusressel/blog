2e24b99c0592701d3e1ab1488012968791872be3
markusressel <markusressel@users.noreply.github.com>
2021-03-31 15:01:43 +0000

    Prettified Code!

--- pages/blog/post/_slug.vue
+++ pages/blog/post/_slug.vue
@@ -12,37 +12,37 @@ export default {
   },
   async asyncData({ $content, error, params }) {
     // fetch article data
-    const article = await getArticle($content, error, params.slug);
+    const article = await getArticle($content, error, params.slug)
 
     // fetch the previous and next article
     let tmp = await $content('articles')
       .only(['title', 'slug'])
       .sortBy('createdAt', 'asc')
-      .fetch();
+      .fetch()
     tmp = tmp.filter((keyword, index) => {
       if (!process.env.NODE_ENV === 'production') {
-        return true;
+        return true
       } else {
-        return tmp[index].dummy === undefined ||
-          tmp[index].dummy === false;
+        return tmp[index].dummy === undefined || tmp[index].dummy === false
       }
-    });
+    })
 
-    let prev = null, next = null;
+    let prev = null,
+      next = null
     for (let i = 0; i <= tmp.length; i++) {
-      let item = tmp[i];
-      if (item.slug  == params.slug) {
+      let item = tmp[i]
+      if (item.slug == params.slug) {
         if (i > 0) {
-          prev = await $content('articles', tmp[i-1].slug)
+          prev = await $content('articles', tmp[i - 1].slug)
             .only(['title', 'slug'])
-            .fetch();
+            .fetch()
         }
         if (i < tmp.length - 1) {
-          next = await $content('articles', tmp[i+1].slug)
+          next = await $content('articles', tmp[i + 1].slug)
             .only(['title', 'slug'])
-            .fetch();
+            .fetch()
         }
-        break;
+        break
       }
     }
 
