846478e3c5797be4de1cb5ce31a972f4ebcafd9c
Markus Ressel <mail@markusressel.de>
2021-04-28 20:33:08 +0200

    fix?

--- utils/productionFilter.js
+++ utils/productionFilter.js
@@ -1,11 +1,13 @@
 export default async (articles) => {
   return articles.filter((keyword, index) => {
-    if (!process.env.NODE_ENV === 'production') {
-      return true
+    if (
+      process.env.NODE_ENV === 'production' &&
+      articles[index].dummy !== undefined &&
+      articles[index].dummy === true
+    ) {
+      return false
     } else {
-      return (
-        articles[index].dummy === undefined || articles[index].dummy === false
-      )
+      return true
     }
   })
 }
