3b28dc26774b09fd660d4353005a8aa821b63ed3
Markus Ressel <mail@markusressel.de>
2022-03-08 17:11:37 +0100

    tag style fix

--- components/Chip.vue
+++ components/Chip.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="px-2 py-1 text-center rounded-full shadow-lg bg-gray-900 tracking-wider font-medium text-ss"
+    class="px-2 py-1 text-center rounded-full shadow-md bg-gray-50 dark:bg-gray-900 tracking-wider font-medium text-ss"
   >
     {{ text }}
   </div>
